429497814ae76ee8d5737275ffaf5988624c5a11
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-10-01 01:23:21 +0300

    added models

---
+++ Movie.Domain/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace Movie.Domain.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    public string ReviewText { get; set; }
+    public int userId { get; set; }
+    public User User { get; set; }
+    public int MovieId  { get; set; }
+    public Movie Movie { get; set; }
+}
---
+++ Movie.Domain/Models/Role.cs
@@ -0,0 +1,7 @@
+﻿namespace Movie.Domain.Models;
+
+public enum Role
+{
+    Admin = 1,
+    User
+}
