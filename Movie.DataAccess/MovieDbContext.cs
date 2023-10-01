d6c6b332c7b2fba810bd2a49e65fe224d260059d
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-10-02 01:15:30 +0300

    added design time factory

--- Movie.DataAccess/MovieDbContext.cs
+++ Movie.DataAccess/MovieDbContext.cs
@@ -5,6 +5,9 @@ namespace Movie.DataAccess;
 
 public class MovieDbContext : DbContext
 {
+    public MovieDbContext(DbContextOptions options) : base (options)
+    { }
+
     public DbSet<User> Users { get; set; }
     public DbSet<Domain.Models.Movie> Movies { get; set; }
     public DbSet<Review> Reviews { get; set; }
---
+++ Movie.DataAccess/MovieDesignTimeFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Movie.DataAccess;
+
+public class MovieDesignTimeFactory : IDesignTimeDbContextFactory<MovieDbContext>
+{
+    public MovieDbContext CreateDbContext(string[] args)
+    {
+        var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        var dbPath = Path.Join(folderPath, "Movie.db");
+
+        var optionBuilder = new DbContextOptionsBuilder<MovieDbContext>();
+        optionBuilder.UseSqlite($"Data Source={dbPath}");   
+        return new MovieDbContext(optionBuilder.Options);    }
+}
