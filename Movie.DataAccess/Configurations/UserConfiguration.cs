47a9846f8ca76f385f55097bc57df4dd889b5860
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-10-02 15:20:21 +0300

    Update UserConfiguration.cs

--- Movie.DataAccess/Configurations/UserConfiguration.cs
+++ Movie.DataAccess/Configurations/UserConfiguration.cs
@@ -1,6 +1,16 @@
-﻿namespace Movie.DataAccess.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Movie.Domain.Models;
 
-public class UserConfiguration
+namespace Movie.DataAccess.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
 {
-    
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).IsRequired();
+        builder.Property(x => x.DateOfBirth).IsRequired();
+        builder.HasMany(x => x.Reviews).WithOne(x => x.User);
+    }
 }
