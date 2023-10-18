5dfbe5e86b9634fe53f2dfc79e70d126a4f231b9
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-10-18 23:17:04 +0300

    web app & tests
    added web app and xUnit

---
+++ Movie.Services.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Movie.Services.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Movie.Services/BaseServiceResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Movie.Services
+{
+    internal class BaseServiceResponse
+    {
+
+    }
+}
