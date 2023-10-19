da955143c643c2752a02c70e88cbbf5cd1af77bc
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-10-20 02:15:19 +0300

    added some views

--- Movie.Services/AuthService.cs
+++ Movie.Services/AuthService.cs
@@ -1,6 +1,18 @@
-﻿namespace Movie.Services
+﻿using Movie.Domain.Abstractions;
+using Movie.Domain.Models;
+
+namespace Movie.Services
 {
-    internal class AuthService
+    public class AuthService : IAuthService
     {
+        public Task<BaseServiceResponse> Login(UserLogin userLogin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseServiceResponse> Register(UserRegistration userRegistration)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
